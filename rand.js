dc612c318e3ccecec013012ba426e33f6c6fbd65
dipangshu22 <dipangshu22@gmail.com>
2025-06-27 21:25:45 +0530

    guess

---
+++ rand.js
@@ -0,0 +1,15 @@
+        
+
+
+
+let a=10
+let user=parseInt(prompt("enter the no"))
+
+if(user===a){
+    console.log("you won")
+}
+else
+{
+    console.log("you loose")
+}
+console.log(getRandomIntExclusive(2))
